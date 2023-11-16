430e768003841fc29cd9f982e19ab43d14f2fd2d
hitesh jaiswal <108576039+Hiteshjaiswall@users.noreply.github.com>
2023-11-16 13:27:10 +0530

    movie cart continued

--- src/Moviecard.js
+++ src/Moviecard.js
@@ -8,7 +8,7 @@ class MovieCard extends React.Component {
             <div className="main">
                 <div className="movie-card">
                     <div className="left">
-                        <img alt="poster" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flipkart.com%2Fmarvel-avengers-wall-poster-room-gloss-lamination-m87-paper-print%2Fp%2Fitm83aceafd50963&psig=AOvVaw17Ka-LM1UsOSdb1tA33SEn&ust=1700112163900000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCMDX2L6hxYIDFQAAAAAdAAAAABAE"/>
+                        <img alt="poster" src="https://rukminim2.flixcart.com/image/850/1000/jxhv1jk0/poster/c/x/v/medium-thor-avengers-endgame-new-poster-for-room-office-endgame-original-imafgyfefdnvuvjh.jpeg?q=90"/>
                     </div>
                     <div className="right">
                         <div className="title"> The Avenegers</div>
@@ -16,7 +16,12 @@ class MovieCard extends React.Component {
                         <div className="price"> Rs. 199 </div>
                         <div className="footer">
                             <div className="rating">8.9</div>
-                            <div className="stars">star </div>
+                            <div className="star-dis">
+                                <img className="str-btn" alt="decarease" src="https://t3.ftcdn.net/jpg/04/22/93/56/240_F_422935625_gTnXZMO6EZZgjOvFDVNMfUFosiMNIppp.jpg"/>
+                                <img className="stars" alt ="stars" src="https://cdn-icons-png.flaticon.com/128/1828/1828884.png" />
+                                <img className="str-btn" alt="increase" src="https://cdn-icons-png.flaticon.com/128/748/748113.png"/>
+                            <span>0</span>
+                            </div>
                             <button className="favourite-btn">Favourtie</button>
                             <button className="cart-btn">Add to cart</button>
                         </div>
