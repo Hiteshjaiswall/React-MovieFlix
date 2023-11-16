3fd492b11aa1af841652ae645eb040fe2e465358
hitesh jaiswal <108576039+Hiteshjaiswall@users.noreply.github.com>
2023-11-16 15:39:56 +0530

    state in react

--- src/Moviecard.js
+++ src/Moviecard.js
@@ -1,9 +1,21 @@
 import React from "react";
-
-
 class MovieCard extends React.Component {
+    // we are uding state to make the program easy so thet we dont have to hard code evertime we are taking a use of the state that is the build in object in the react 
+    // single sourse of truth coming soon we will use the signgle sourse
+
+    constructor(){
+        super();
+        this.state={
+            title:"the avengers",
+            plot:"fighting to save the world ",
+            price:199,
+            rating:8.9,
+        }
+    }
     //  use render function 
     render() {
+//  important here we are destructuring the object 
+        const {title, plot, rating, price}=this.state;
         return (
             <div className="main">
                 <div className="movie-card">
@@ -11,11 +23,11 @@ class MovieCard extends React.Component {
                         <img alt="poster" src="https://rukminim2.flixcart.com/image/850/1000/jxhv1jk0/poster/c/x/v/medium-thor-avengers-endgame-new-poster-for-room-office-endgame-original-imafgyfefdnvuvjh.jpeg?q=90"/>
                     </div>
                     <div className="right">
-                        <div className="title"> The Avenegers</div>
-                        <div className="plot"> fighting to save world</div>
-                        <div className="price"> Rs. 199 </div>
+                        <div className="title"> {title}</div>
+                        <div className="plot"> {plot}</div>
+                        <div className="price"> {price} </div>
                         <div className="footer">
-                            <div className="rating">8.9</div>
+                            <div className="rating">{rating}</div>
                             <div className="star-dis">
                                 <img className="str-btn" alt="decarease" src="https://t3.ftcdn.net/jpg/04/22/93/56/240_F_422935625_gTnXZMO6EZZgjOvFDVNMfUFosiMNIppp.jpg"/>
                                 <img className="stars" alt ="stars" src="https://cdn-icons-png.flaticon.com/128/1828/1828884.png" />
@@ -31,6 +43,4 @@ class MovieCard extends React.Component {
         )
     }
 }
-
-
 export default MovieCard;
